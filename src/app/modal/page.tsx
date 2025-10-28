4b2093bcc335559702b5625f49703a8206572049
Mohanaprabhu.R <mohan@timeless.co>
2025-10-28 14:20:05 +0530

    feat: enhance modal demo with share dialog and improve dialog content styling

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -45,7 +45,7 @@ const modalDemo = () => {
             <DialogTrigger asChild>
               <Button variant="outline">Open Dialog</Button>
             </DialogTrigger>
-            <DialogContent size="md" className="space-y-4">
+            <DialogContent size="md">
               <DialogHeader className="flex flex-col gap-y-4">
                 <DialogTitle>Title</DialogTitle>
               </DialogHeader>
@@ -152,6 +152,38 @@ const modalDemo = () => {
                     Save changes
                   </Button>
                 </ButtonGroup>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Share</Button>
+            </DialogTrigger>
+            <DialogContent className="gap-4 ">
+              <DialogHeader>
+                <DialogTitle>Share link</DialogTitle>
+                <DialogDescription>
+                  Anyone who has this link will be able to view this.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex items-center gap-2">
+                <div className="grid flex-1 gap-2">
+                  <Label htmlFor="link" className="sr-only">
+                    Link
+                  </Label>
+                  <Input
+                    id="link"
+                    defaultValue="https://ui.shadcn.com/docs/installation"
+                    readOnly
+                  />
+                </div>
+              </div>
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary">
+                    Close
+                  </Button>
+                </DialogClose>
               </DialogFooter>
             </DialogContent>
           </Dialog>
