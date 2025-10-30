20308e623bf75a1ca69bb758dd32c62a1fdedcd0
Mohanaprabhu.R <mohan@timeless.co>
2025-10-30 16:22:35 +0530

    All Component line height and letter spacing fixed - calender

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -90,7 +90,7 @@ function Calendar({
           "text-muted-foreground rounded-md flex-1 font-medium text-xs select-none mb-1.5 tracking-4 leading-loose",
           defaultClassNames.weekday
         ),
-        week: cn("flex w-full gap-x-2.5 my-2.5", defaultClassNames.week),
+        week: cn("flex w-full gap-x-2 my-2", defaultClassNames.week),
         week_number_header: cn(
           "select-none w-(--cell-size)",
           defaultClassNames.week_number_header
