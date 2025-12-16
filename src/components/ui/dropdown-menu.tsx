a432244e6c2c9b9ced03ef2fcc9520f522ef804f
Mohanaprabhu.R <mohan@timeless.co>
2025-12-16 11:03:24 +0530

    fix(dropdown-menu): remove redundant class for improved clarity

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -269,7 +269,7 @@ function DropdownMenuSubTrigger({
       data-slot="dropdown-menu-sub-trigger"
       data-inset={inset}
       className={cn(
-        "focus:bg-accent [&_svg]:size-4 focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-lg px-2 py-1.5 text-base font-normal gap-2 leading-tight  tracking-4 outline-hidden  select-none data-[inset]:pl-8",
+        "focus:bg-accent  focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-lg px-2 py-1.5 text-base font-normal gap-2 leading-tight  tracking-4 outline-hidden  select-none data-[inset]:pl-8",
         className
       )}
       {...props}
